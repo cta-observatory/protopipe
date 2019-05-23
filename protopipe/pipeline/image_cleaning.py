2d11a0cc7dbd1ef3eba60e4cce20dbf7326dfbb0
Michele Peresano <michele.peresano@.cea.fr>
2019-05-23 11:40:08 +0200

    Temporarely added MARS cleaning 1st pass, corrected bug in tailcut boundary threshold selection, some format PEP8 auto-corrections

--- protopipe/pipeline/image_cleaning.py
+++ protopipe/pipeline/image_cleaning.py
@@ -13,7 +13,49 @@ except ImportError as e:
     print("wavelet cleaning will not work")
     print(e)
 
-__all__ = ['ImageCleaner']
+__all__ = ["ImageCleaner"]
+
+
+def mars_image_cleaning(
+    geom,
+    image,
+    picture_thresh=7,
+    boundary_thresh=5,
+    keep_isolated_pixels=False,
+    min_number_picture_neighbors=0,
+):
+
+    """This is here temporarely, just to launch a production without waiting
+    for the official ctapipe pull-request, which will be done in parallel.
+    More details in ctapipe.image.cleaning in the
+    updated local version (ask Michele P.)"""
+
+    pixels_from_tailcuts_clean = tailcuts_clean(
+        geom,
+        image,
+        picture_thresh,
+        boundary_thresh,
+        keep_isolated_pixels,
+        min_number_picture_neighbors,
+    )  # this does checks for core pixels and first neighbors
+
+    # MARS has the same threshold for both boundary_thresh
+    pixels_above_2nd_boundary = image >= boundary_thresh
+
+    pixels_with_previous_neighbors = geom.neighbor_matrix_sparse.dot(
+        pixels_from_tailcuts_clean
+    )
+    if keep_isolated_pixels:
+        return (
+            pixels_above_2nd_boundary & pixels_with_previous_neighbors
+        ) | pixels_from_tailcuts_clean
+    else:
+        pixels_with_2ndboundary_neighbors = geom.neighbor_matrix_sparse.dot(
+            pixels_above_2nd_boundary
+        )
+        return (pixels_above_2nd_boundary & pixels_with_previous_neighbors) | (
+            pixels_from_tailcuts_clean & pixels_with_2ndboundary_neighbors
+        )
 
 
 class ImageCleaner(object):
@@ -27,7 +69,7 @@ class ImageCleaner(object):
         Model corresponding to `wave` or `tail`
     """
 
-    def __init__(self, config, mode='tail'):
+    def __init__(self, config, mode="tail"):
 
         self.config = config
         self.mode = mode
@@ -40,58 +82,68 @@ class ImageCleaner(object):
         self.clean_opts = {}  # Options for cleaners for corresp. camera
         opt = self.config[self.mode]
 
-        if self.mode in 'tail':
+        if self.mode in "tail":
 
             # Initialise cleaner per camera type
-            for type in opt['thresholds']:
+            for type in opt["thresholds"]:
                 cam_id = list(type.keys())[0]
                 cuts = list(type.values())[0]
 
                 self.clean_opts[cam_id] = {
-                    'picture_thresh': cuts[0],
-                    'boundary_thresh': cuts[0],
-                    'keep_isolated_pixels': opt['keep_isolated_pixels'],
-                    'min_number_picture_neighbors': opt['min_number_picture_neighbors']
+                    "picture_thresh": cuts[0],
+                    "boundary_thresh": cuts[1],
+                    "keep_isolated_pixels": opt["keep_isolated_pixels"],
+                    "min_number_picture_neighbors": opt["min_number_picture_neighbors"],
                 }
 
                 # print(type)
                 # print(cam_id)
                 # print(cuts)
 
-                self.cleaners[cam_id] = lambda img, geom, opt: \
-                    tailcuts_clean(image=img, geom=geom, **opt)
+                # we don't want to use the old tailcut ....
+                # self.cleaners[cam_id] = lambda img, geom, opt: tailcuts_clean(
+                # image=img, geom=geom, **opt
+                # )
 
-        elif self.mode in 'wave':
+                # ... we want to use new MARS 1st pass function
+                self.cleaners[cam_id] = lambda img, geom, opt: mars_image_cleaning(
+                    image=img, geom=geom, **opt
+                )
+
+        elif self.mode in "wave":
 
             # Initialise cleaner per camera type
-            for cam_id in opt['options']:
-                opt_type = opt['options'][cam_id]
+            for cam_id in opt["options"]:
+                opt_type = opt["options"][cam_id]
 
                 # WARNING WARNING WARNING
                 # camera models for noise injection
                 # Need to be updated if calib/signal integration is changed!!!!
-                self.noise_model = \
-                    {"ASTRICam": EmpiricalDistribution(cdf.ASTRI_CDF_FILE),
-                     "DigiCam": EmpiricalDistribution(cdf.DIGICAM_CDF_FILE),
-                     "FlashCam": EmpiricalDistribution(cdf.FLASHCAM_CDF_FILE),
-                     "NectarCam": EmpiricalDistribution(cdf.NECTARCAM_CDF_FILE),
-                     "LSTCam": EmpiricalDistribution(cdf.LSTCAM_CDF_FILE),
-                     "CHEC": EmpiricalDistribution(cdf.DIGICAM_CDF_FILE)}
-
-                self.clean_opts[cam_id] = {
-                    'type_of_filtering': opt_type['type_of_filtering'],
-                    'filter_thresholds': opt_type['filter_thresholds'],
-                    'last_scale_treatment': opt_type['last_scale_treatment'],
-                    'kill_isolated_pixels': opt_type['kill_isolated_pixels'],
-                    'detect_only_positive_structures': opt_type[
-                        'detect_only_positive_structures'],
-                    'tmp_files_directory': opt['tmp_files_directory'],
-                    'clusters_threshold': opt_type['clusters_threshold'],
-                    'noise_distribution': self.noise_model[cam_id]
+                self.noise_model = {
+                    "ASTRICam": EmpiricalDistribution(cdf.ASTRI_CDF_FILE),
+                    "DigiCam": EmpiricalDistribution(cdf.DIGICAM_CDF_FILE),
+                    "FlashCam": EmpiricalDistribution(cdf.FLASHCAM_CDF_FILE),
+                    "NectarCam": EmpiricalDistribution(cdf.NECTARCAM_CDF_FILE),
+                    "LSTCam": EmpiricalDistribution(cdf.LSTCAM_CDF_FILE),
+                    "CHEC": EmpiricalDistribution(cdf.DIGICAM_CDF_FILE),
                 }
 
-                self.cleaners[cam_id] = lambda img, opt: \
-                    WaveletTransform().clean_image(input_image=img, **opt)
+                self.clean_opts[cam_id] = {
+                    "type_of_filtering": opt_type["type_of_filtering"],
+                    "filter_thresholds": opt_type["filter_thresholds"],
+                    "last_scale_treatment": opt_type["last_scale_treatment"],
+                    "kill_isolated_pixels": opt_type["kill_isolated_pixels"],
+                    "detect_only_positive_structures": opt_type[
+                        "detect_only_positive_structures"
+                    ],
+                    "tmp_files_directory": opt["tmp_files_directory"],
+                    "clusters_threshold": opt_type["clusters_threshold"],
+                    "noise_distribution": self.noise_model[cam_id],
+                }
+
+                self.cleaners[cam_id] = lambda img, opt: WaveletTransform().clean_image(
+                    input_image=img, **opt
+                )
 
     def clean_image(self, img, geom):
         """Clean image according to configuration
@@ -112,13 +164,13 @@ class ImageCleaner(object):
         new_img = np.copy(img)
         cam_id = geom.cam_id
 
-        if self.mode in 'tail':
+        if self.mode in "tail":
             mask = self.cleaners[cam_id](new_img, geom, self.clean_opts[cam_id])
             new_img[~mask] = 0
 
             return new_img
 
-        if self.mode in 'wave':
+        if self.mode in "wave":
             image_2d = geometry_converter.image_1d_to_2d(new_img, cam_id)
             image_2d = self.cleaners[cam_id](image_2d, self.clean_opts[cam_id])
             new_img = geometry_converter.image_2d_to_1d(image_2d, cam_id)
