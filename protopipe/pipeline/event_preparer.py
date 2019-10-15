73cf03610ae83ef9957aa21aa334340149867243
Michele Peresano <michele.peresano@.cea.fr>
2019-10-15 18:39:05 +0200

    Tailcut-based cleaning in full ctapipe 0.7; wavelet cleaning optional and separated using previous pywi-cta code.

--- protopipe/pipeline/event_preparer.py
+++ protopipe/pipeline/event_preparer.py
@@ -307,31 +307,69 @@ class EventPreparer:
                     dl1_phe_image = pmt_signal
                     mc_phe_image = event.mc.tel[tel_id].photo_electron_image
 
-                # Clean the image:
-                # for the moment we use pywi-cta functionalities to make
-                # conversions between 1D & 2D images.
-                # We'll switch soon to ctapipe also here.
-                try:
-                    with warnings.catch_warnings():
-                        # Image with biggest cluster (reco cleaning)
-                        image_biggest = self.cleaner_reco.clean_image(
-                            pmt_signal, camera
-                        )
-                        image_biggest2d = geometry_converter.image_1d_to_2d(
-                            image_biggest, camera.cam_id
-                        )
-                        image_biggest2d = filter_pixels_clusters(image_biggest2d)
-                        image_biggest = geometry_converter.image_2d_to_1d(
-                            image_biggest2d, camera.cam_id
-                        )
+                if self.cleaner_reco.mode == "tail":  # tail uses only ctapipe
 
-                        # Image for score/energy estimation (with clusters)
-                        image_extended = self.cleaner_extended.clean_image(
-                            pmt_signal, camera
-                        )
-                except FileNotFoundError as e:  # JLK, WHAT?
-                    print(e)
-                    continue
+                    # Cleaning used for direction reconstruction
+                    image_biggest, mask_reco = self.cleaner_reco.clean_image(
+                        pmt_signal, camera
+                    )
+                    # find all islands (in this case we don't care how many)
+                    _, labels = number_of_islands(camera, mask_reco)
+                    # find biggest one
+                    mask_biggest = largest_island(labels)
+                    # filter camera geometry and calibrated image for it
+                    camera_biggest = camera[mask_biggest]
+                    image_biggest = image_biggest[mask_biggest]
+
+                    # Cleaning used for score/energy estimation
+                    image_extended, mask_extended = self.cleaner_extended.clean_image(
+                        pmt_signal, camera
+                    )
+                    # find all islands (in THIS case we care how many)
+                    n_cluster_dict[tel_id], labels = number_of_islands(camera, mask_reco)
+                    # filter camera geometry and calibrated image
+                    camera_extended = camera[mask_extended]
+                    image_extended = image_extended[mask_extended]
+
+                else:  # for wavelets we stick to old pywi-cta code
+                    try:  # "try except FileNotFoundError" not clear to me, but for now it stays...
+                        with warnings.catch_warnings():
+                            # Image with biggest cluster (reco cleaning)
+                            image_biggest, mask_reco = self.cleaner_reco.clean_image(
+                                pmt_signal, camera
+                            )
+                            image_biggest2d = geometry_converter.image_1d_to_2d(
+                                image_biggest, camera.cam_id
+                            )
+                            image_biggest2d = filter_pixels_clusters(image_biggest2d)
+                            image_biggest = geometry_converter.image_2d_to_1d(
+                                image_biggest2d, camera.cam_id
+                            )
+
+                            # Image for score/energy estimation (with clusters)
+                            image_extended, mask_extended = self.cleaner_extended.clean_image(
+                                pmt_signal, camera
+                            )
+
+                            # This last part was outside the pywi-cta block
+                            # before, but is indeed part of it because it uses
+                            # pywi-cta functions in the "extended" case
+
+                            # For cluster counts
+                            image_2d = geometry_converter.image_1d_to_2d(
+                                image_extended, camera.cam_id
+                            )
+                            n_cluster_dict[tel_id] = pixel_clusters.number_of_pixels_clusters(
+                                array=image_2d, threshold=0
+                            )
+                            # could this go into `hillas_parameters` ...?
+                            max_signals[tel_id] = np.max(image_extended)
+
+                    except FileNotFoundError as e:  # JLK, WHAT?
+                        print(e)
+                        continue
+
+                # ==============================================================
 
                 # Apply some selection
                 if self.image_cutflow.cut("min pixel", image_biggest):
@@ -339,17 +377,6 @@ class EventPreparer:
 
                 if self.image_cutflow.cut("min charge", np.sum(image_biggest)):
                     continue
-
-                # For cluster counts
-                image_2d = geometry_converter.image_1d_to_2d(
-                    image_extended, camera.cam_id
-                )
-                n_cluster_dict[tel_id] = pixel_clusters.number_of_pixels_clusters(
-                    array=image_2d, threshold=0
-                )
-
-                # could this go into `hillas_parameters` ...?
-                max_signals[tel_id] = np.max(image_extended)
 
                 # do the hillas reconstruction of the images
                 # QUESTION should this change in numpy behaviour be done here
