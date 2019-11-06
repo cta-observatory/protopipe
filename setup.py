d059c341a78059c29f8c2daf1c5410692beb2f64
Michele Peresano <michele.peresano@.cea.fr>
2019-11-06 14:54:37 +0100

    Added versioning to init.py and setup.py using the manual approach.

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, find_packages
 
+
 def readme():
-    with open('README.rst') as f:
+    with open("README.rst") as f:
         return f.read()
 
-setup(name='protopipe',
-      version='0.1',
-      description='Pipeline to process events from DL0 to DL3',
-      url='http://github.com/jjlk/protopipe',
-      author='CEA',
-      author_email='julien.lefaucheur@cea.fr',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=['ctapipe'],
-      zip_safe=False)
+
+setup(
+    name="protopipe",
+    version="0.2.1-dev",
+    description="Pipeline to process events from DL0 to DL3",
+    url="https://github.com/cta-observatory/protopipe",
+    author="CEA",
+    author_email="michele.peresano@cea.fr",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["ctapipe"],
+    zip_safe=False,
+)
