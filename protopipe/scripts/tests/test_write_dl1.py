2d6c6051014d22ba64a46203d4a77931a398d24b
Michele Peresano <michele.peresano@.cea.fr>
2019-11-15 15:22:57 +0100

    Added first test function for write_dl1.py

---
+++ protopipe/scripts/tests/test_write_dl1.py
@@ -0,0 +1,22 @@
+"""Test the write_dl1 script."""
+import os
+import protopipe
+from protopipe.scripts import write_dl1
+from ctapipe.utils import get_dataset_path
+
+GAMMA_TEST_LARGE = get_dataset_path("gamma_test_large.simtel.gz")
+parentdir = os.path.dirname(protopipe.__path__[0])
+configs = os.path.join(parentdir, "aux/example_config_files/protopipe/")
+
+
+def test_write_dl1():
+    """Very bare test to see if the script reaches the end correctly."""
+    exit_status = os.system(
+        f"python {write_dl1.__file__}\
+        --config_file {os.path.join(configs, 'analysis.yaml')}\
+        -o test_dl1.h5\
+        -i {os.path.dirname(GAMMA_TEST_LARGE)}\
+        -f {os.path.basename(GAMMA_TEST_LARGE)}\
+        -m 10"
+    )
+    assert exit_status == 0
