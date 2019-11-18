f4806475bbad95b51f9442c3d0bf631de83610d0
Michele Peresano <michele.peresano@.cea.fr>
2019-11-18 14:33:03 +0100

    Specify tested insturments and test file limitations.

--- protopipe/scripts/tests/test_write_dl1.py
+++ protopipe/scripts/tests/test_write_dl1.py
@@ -4,13 +4,21 @@ import protopipe
 from protopipe.scripts import write_dl1
 from ctapipe.utils import get_dataset_path
 
+# TEST FILES
+# Instruments tested: LST_LST_LSTCam, MST_MST_FlashCam, SST_ASTRI_ASTRICam
 GAMMA_TEST_LARGE = get_dataset_path("gamma_test_large.simtel.gz")
+
 parentdir = os.path.dirname(protopipe.__path__[0])
 configs = os.path.join(parentdir, "aux/example_config_files/protopipe/")
 
 
 def test_write_dl1():
-    """Very bare test to see if the script reaches the end correctly."""
+    """Very bare test to see if the script reaches the end correctly.
+
+    WARNING: this is unfortunately not sufficient, since we work with many
+    different simulation setups, but for the moment in protopipe we provide
+    automatic tests made only using GAMMA_TEST_LARGE.
+    """
     exit_status = os.system(
         f"python {write_dl1.__file__}\
         --config_file {os.path.join(configs, 'analysis.yaml')}\
