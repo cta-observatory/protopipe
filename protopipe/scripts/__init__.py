88ef91e8f71fc13f0175fd64c782c3fe0f24cad9
Michele Peresano <michele.peresano@cea.fr>
2020-07-30 16:53:00 +0200

    Remove obsolete write_dl1 material.

--- protopipe/scripts/__init__.py
+++ protopipe/scripts/__init__.py
@@ -3,7 +3,8 @@ Scripts to process and write events with ctapipe
 """
 
 from .data_training import *
-from .write_dl1 import *
+
+# from .write_dl1 import *
 from .write_dl2 import *
 from .build_model import *
 from .model_diagnostic import *
