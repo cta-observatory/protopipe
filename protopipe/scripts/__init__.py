546372ca2a203105f3c3afd62fe705490f91c2f3
gaia-verna <verna@cppm.in2p3.fr>
2020-12-15 17:14:32 +0100

    Update name in __init__.py

--- protopipe/scripts/__init__.py
+++ protopipe/scripts/__init__.py
@@ -8,4 +8,4 @@ from .data_training import *
 from .write_dl2 import *
 from .build_model import *
 from .model_diagnostic import *
-from .make_performance import *
+from .make_performance_EventDisplay import *
