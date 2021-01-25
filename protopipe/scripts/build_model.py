e5b64bb6ddd3e51e7b0b6234e7594f048b4252b0
Michele Peresano <michele.peresano@cea.fr>
2021-01-25 17:28:42 +0100

    Started improving model script (see complete commit message for details)
    - refactored I/O from protopipe.mva.io
    - add Random Forest energy regressor
    - explicit all class options from scikit-learn
    - better organized information
    - smaller fixes

--- protopipe/scripts/build_model.py
+++ protopipe/scripts/build_model.py
@@ -2,92 +2,129 @@
 
 import os
 import pandas as pd
-import argparse
 from os import path
 from sklearn.ensemble import (
     AdaBoostRegressor,
     AdaBoostClassifier,
     RandomForestClassifier,
+    RandomForestRegressor,
 )
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
-import joblib
 from sklearn.metrics import classification_report
 from sklearn.calibration import CalibratedClassifierCV
 
 from protopipe.pipeline.utils import load_config
 
 from protopipe.mva import TrainModel
-from protopipe.mva.utils import make_cut_list, prepare_data, save_obj
+from protopipe.mva.io import initialize_script_arguments, save_output
+from protopipe.mva.utils import (
+    make_cut_list,
+    prepare_data
+)
 
 
 def main():
 
-    # Read arguments
-    parser = argparse.ArgumentParser(
-        description="Build model for regression/classification"
-    )
-    parser.add_argument("--config_file", type=str, required=True)
-    parser.add_argument(
-        "--max_events",
-        type=int,
-        default=-1,
-        help="maximum number of events for training",
-    )
-    mode_group = parser.add_mutually_exclusive_group()
-    mode_group.add_argument(
-        "--wave",
-        dest="mode",
-        action="store_const",
-        const="wave",
-        default="tail",
-        help="if set, use wavelet cleaning",
-    )
-    mode_group.add_argument(
-        "--tail",
-        dest="mode",
-        action="store_const",
-        const="tail",
-        help="if set, use tail cleaning, otherwise wavelets",
-    )
-    args = parser.parse_args()
+    args = initialize_script_arguments()
 
-    # Read configuration file
+    # LOAD CONFIGURATION FILE
     cfg = load_config(args.config_file)
 
-    # Type of model (regressor or classifier)
-    model_type = cfg["General"]["model_type"]
+    # INPUT CONFIGURATION
 
-    # Import parameters
+    # I/O settings
     data_dir = cfg["General"]["data_dir"]
     outdir = cfg["General"]["outdir"]
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-
+    # Types of cameras
     cam_ids = cfg["General"]["cam_id_list"]
-    table_name_template = cfg["General"]["table_name_template"]
-    table_name = [table_name_template + cam_id for cam_id in cam_ids]
-
+    table_name = [cam_id for cam_id in cam_ids]
+    # Type of model (regression or classification)
+    model_type = cfg["General"]["model_type"]
+    # Dataset split train-test fraction
+    train_fraction = cfg["Split"]["train_fraction"]
     # List of features
     feature_list = cfg["FeatureList"]
-
-    # Optimisation parameters
     method_name = cfg["Method"]["name"]
+
+    # GridSearchCV
+    use_GridSearchCV = cfg["GridSearchCV"]["use"]
+    scoring = cfg["GridSearchCV"]["scoring"]
+    cv = cfg["GridSearchCV"]["cv"]
+
+    # This is a list of dictionaries to generalize for GridSearchCV
+    # The initial model will be always initialized with the first set of
+    # tuned_parameters
     tuned_parameters = [cfg["Method"]["tuned_parameters"]]
-    scoring = cfg["Method"]["scoring"]
-    cv = cfg["Method"]["cv"]
 
-    # Split fraction
-    train_fraction = cfg["Split"]["train_fraction"]
+    # Hyper-parameters based on type of model
 
-    if model_type in "regressor":
-        data_file = cfg["General"]["data_file"].format(args.mode)
+    if method_name in ["RandomForestRegressor", "RandomForestClassifier"]:
+        n_estimators = tuned_parameters[0]["n_estimators"]
+        criterion = tuned_parameters[0]["criterion"]
+        max_depth = None if tuned_parameters[0]["max_depth"] == "None" else tuned_parameters[0]["max_depth"]
+        min_samples_split = tuned_parameters[0]["min_samples_split"]
+        min_samples_leaf = tuned_parameters[0]["min_samples_leaf"]
+        min_weight_fraction_leaf = tuned_parameters[0]["min_weight_fraction_leaf"]
+        max_features = tuned_parameters[0]["max_features"]
+        max_leaf_nodes = None if tuned_parameters[0]["max_leaf_nodes"] == "None" else tuned_parameters[0]["max_leaf_nodes"]
+        min_impurity_decrease = tuned_parameters[0]["min_impurity_decrease"]
+        bootstrap = False if tuned_parameters[0]["bootstrap"] == "False" else True
+        oob_score = False if tuned_parameters[0]["oob_score"] == "False" else True
+        n_jobs = None if tuned_parameters[0]["n_jobs"] == "None" else tuned_parameters[0]["n_jobs"]
+        random_state = None if tuned_parameters[0]["random_state"] == "None" else tuned_parameters[0]["random_state"]
+        verbose = tuned_parameters[0]["verbose"]
+        warm_start = False if tuned_parameters[0]["warm_start"] == "False" else True
+        ccp_alpha = tuned_parameters[0]["ccp_alpha"]
+        max_samples = None if tuned_parameters[0]["max_samples"] == "None" else tuned_parameters[0]["max_samples"]
+
+    if method_name == 'AdaBoostRegressor':
+        base_estimator = 'None'  # (aka DecisionTreeRegressor)
+        n_estimators = cfg["Method"]["n_estimators"]
+        learning_rate = cfg["Method"]["learning_rate"]
+        random_state = None if cfg["Method"]["random_state"] == "None" else cfg["Method"]["random_state"]
+
+    if method_name == 'RandomForestClassifier':
+        class_weight = cfg["Method"]["class_weight"]
+
+    if model_type == "regressor":
+        data_file = cfg["General"]["data_file"]
         filename = path.join(data_dir, data_file)
 
-        # List of cuts
+        # Get the selection cuts
         cuts = make_cut_list(cfg["SigFiducialCuts"])
-        init_model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=None))
 
-        # Name of target
+        # # Initialize the model
+        if method_name in "AdaBoostRegressor":
+            init_model = AdaBoostRegressor(
+                base_estimator=DecisionTreeRegressor(max_depth=None)
+                )
+        if method_name in "RandomForestRegressor":
+            init_model = RandomForestRegressor(
+                n_estimators=n_estimators,
+                criterion=criterion,
+                max_depth=max_depth,
+                min_samples_split=min_samples_split,
+                min_samples_leaf=min_samples_leaf,
+                min_weight_fraction_leaf=min_weight_fraction_leaf,
+                max_features=max_features,
+                max_leaf_nodes=max_leaf_nodes,
+                min_impurity_decrease=min_impurity_decrease,
+                bootstrap=bootstrap,
+                oob_score=oob_score,
+                n_jobs=n_jobs,
+                random_state=random_state,
+                verbose=verbose,
+                warm_start=warm_start,
+                ccp_alpha=ccp_alpha,
+                max_samples=max_samples
+            )
+        else:
+            print("ERROR: we support only AdaBoostRegressor and RandomForestRegressor at the moment!")
+            exit()
+
+        # Name of target quantity
         target_name = cfg["Method"]["target_name"]
 
     elif model_type in "classifier":
@@ -96,23 +133,33 @@ def main():
         filename_sig = path.join(data_dir, data_sig_file)
         filename_bkg = path.join(data_dir, data_bkg_file)
 
-        # List of cuts
+        # Get the selection cuts
         sig_cuts = make_cut_list(cfg["SigFiducialCuts"])
         bkg_cuts = make_cut_list(cfg["BkgFiducialCuts"])
 
-        # Model
+        # Initialize the the base estimator
         if method_name in "AdaBoostClassifier":
             init_model = AdaBoostClassifier(DecisionTreeClassifier(max_depth=4))
         elif method_name in "RandomForestClassifier":
             init_model = RandomForestClassifier(
-                n_estimators=500,
-                max_depth=None,
-                min_samples_split=0.05,
-                max_features="sqrt",
-                bootstrap=True,
-                random_state=None,
-                criterion="gini",
-                class_weight="balanced_subsample",  # Tree-wise re-weighting
+                n_estimators=n_estimators,
+                criterion=criterion,
+                max_depth=max_depth,
+                min_samples_split=min_samples_split,
+                min_samples_leaf=min_samples_leaf,
+                min_weight_fraction_leaf=min_weight_fraction_leaf,
+                max_features=max_features,
+                max_leaf_nodes=max_leaf_nodes,
+                min_impurity_decrease=min_impurity_decrease,
+                bootstrap=bootstrap,
+                oob_score=oob_score,
+                n_jobs=n_jobs,
+                random_state=random_state,
+                verbose=verbose,
+                warm_start=warm_start,
+                class_weight=class_weight,
+                ccp_alpha=ccp_alpha,
+                max_samples=max_samples
             )
 
             # Name of target
@@ -130,19 +177,24 @@ def main():
         print("### Building model for {}".format(cam_id))
 
         if model_type in "regressor":
+
             # Load data
             data = pd.read_hdf(filename, table_name[idx], mode="r")
-            data = prepare_data(ds=data, cuts=cuts)[0 : args.max_events]
+            data = prepare_data(ds=data, cuts=cuts)[0: args.max_events]
 
-            # Init model factory
+            # Initialize the model
             factory = TrainModel(
-                case=model_type, target_name=target_name, feature_name_list=feature_list
+                case=model_type,
+                target_name=target_name,
+                feature_name_list=feature_list
             )
 
-            # Split data
+            # Split the TRAINING dataset in a train and test sub-datasets
+            # Useful to test the models before using them for DL2 production
             factory.split_data(data_sig=data, train_fraction=train_fraction)
             print("Training sample: sig {}".format(len(factory.data_train)))
             print("Test sample: sig {}".format(len(factory.data_test)))
+
         elif model_type in "classifier":
             # Load data
             data_sig = pd.read_hdf(filename_sig, table_name[idx], mode="r")
@@ -152,15 +204,16 @@ def main():
             data_sig = prepare_data(ds=data_sig, label=1, cuts=sig_cuts)
             data_bkg = prepare_data(ds=data_bkg, label=0, cuts=bkg_cuts)
 
-            data_sig = data_sig[0 : args.max_events]
-            data_bkg = data_bkg[0 : args.max_events]
+            data_sig = data_sig[0: args.max_events]
+            data_bkg = data_bkg[0: args.max_events]
 
-            # Init model factory
+            # Initialize the model
             factory = TrainModel(
                 case=model_type, target_name=target_name, feature_name_list=feature_list
             )
 
-            # Split data
+            # Split the TRAINING dataset in a train and test sub-datasets
+            # Useful to test the models before using them for DL2 production
             factory.split_data(
                 data_sig=data_sig,
                 data_bkg=data_bkg,
@@ -181,20 +234,22 @@ def main():
                 )
             )
 
-        # Build model
-        best_model = factory.get_optimal_model(
-            init_model, tuned_parameters, scoring=scoring, cv=cv
-        )
+        # Apply optimization of the hyper-parameters via grid search if enabled
+        if use_GridSearchCV == "True":
+            best_model = factory.get_optimal_model(
+                init_model, tuned_parameters, scoring=scoring, cv=cv
+            )
+        else:  # otherwise use directly the initial model
+            best_model = init_model
 
         if model_type in "classifier":
-            # print report
-            if model_type in "classifier":
-                print(
-                    classification_report(
-                        factory.data_scikit["y_test"],
-                        best_model.predict(factory.data_scikit["X_test"]),
-                    )
+
+            print(
+                classification_report(
+                    factory.data_scikit["y_test"],
+                    best_model.predict(factory.data_scikit["X_test"]),
                 )
+            )
 
             # Calibrate model if necessary on test data
             if cfg["Method"]["calibrate_output"] is True:
@@ -205,42 +260,17 @@ def main():
                 )
 
                 best_model.fit(
-                    factory.data_scikit["X_test"], factory.data_scikit["y_test"]
+                    factory.data_scikit["X_test"],
+                    factory.data_scikit["y_test"]
                 )
 
-        # save model
-        models[cam_id] = best_model
-        outname = "{}_{}_{}_{}.pkl.gz".format(
-            model_type, args.mode, cam_id, method_name
-        )
-        joblib.dump(best_model, path.join(outdir, outname))
-
-        # save data
-        save_obj(
-            factory.data_scikit,
-            path.join(
-                outdir,
-                "data_scikit_{}_{}_{}_{}.pkl.gz".format(
-                    model_type, method_name, args.mode, cam_id
-                ),
-            ),
-        )
-        factory.data_train.to_pickle(
-            path.join(
-                outdir,
-                "data_train_{}_{}_{}_{}.pkl.gz".format(
-                    model_type, method_name, args.mode, cam_id
-                ),
-            )
-        )
-        factory.data_test.to_pickle(
-            path.join(
-                outdir,
-                "data_test_{}_{}_{}_{}.pkl.gz".format(
-                    model_type, method_name, args.mode, cam_id
-                ),
-            )
-        )
+        save_output(models,
+                    cam_id,
+                    factory,
+                    best_model,
+                    model_type,
+                    method_name,
+                    outdir)
 
 
 if __name__ == "__main__":
