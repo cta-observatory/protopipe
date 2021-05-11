bbc2cc4c2c0213d888921c560411e11f6846bf4f
Michele Peresano <michele.peresano@cea.fr>
2021-05-11 10:20:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ extras_require = {
         "gammapy",
         "pytest",
         "numpy",
-        "ctapipe==0.10.5",
+        "ctapipe==0.11.0",
         "pyirf",
     ],
     "tests": [
@@ -43,7 +43,7 @@ setup(
     packages=find_packages(),
     package_data={"protopipe": ["aux/example_config_files/analysis.yaml"]},
     include_package_data=True,
-    install_requires=["ctapipe==0.10.5", "pyirf"],
+    install_requires=["ctapipe==0.11.0", "pyirf"],
     zip_safe=False,
     use_scm_version={"write_to": os.path.join("protopipe", "_version.py")},
     tests_require=extras_require["tests"],
