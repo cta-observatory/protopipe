d781b532ae24f02969f067e249afa3b63b6a7a8a
Michele Peresano <michele.peresano@cea.fr>
2021-06-16 12:11:58 +0200

    remove old calibscale from event preparer

--- protopipe/pipeline/event_preparer.py
+++ protopipe/pipeline/event_preparer.py
@@ -145,13 +145,6 @@ class EventPreparer:
     ):
         """Initiliaze an EventPreparer object."""
 
-        # Calibscale
-        try:
-            self.calibscale = config["Calibration"]["calibscale"]
-        except KeyError:
-            # defaults for no calibscale applied
-            self.calibscale = 1.0
-
         # Cleaning for reconstruction
         self.cleaner_reco = ImageCleaner(  # for reconstruction
             config=config["ImageCleaning"]["biggest"],
@@ -467,7 +460,7 @@ class EventPreparer:
 
                 # use ctapipe's functionality to get the calibrated image
                 # and scale the reconstructed values if required
-                pmt_signal = event.dl1.tel[tel_id].image / self.calibscale
+                pmt_signal = event.dl1.tel[tel_id].image
 
                 # If required...
                 if save_images is True:
