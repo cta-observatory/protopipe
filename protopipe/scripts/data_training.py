71abd398fc961fcc2420bd860ea52f3faf4d19a0
Michele Peresano <michele.peresano@cea.fr>
2021-06-16 12:07:56 +0200

    Use new calibscale in data_training script

--- protopipe/scripts/data_training.py
+++ protopipe/scripts/data_training.py
@@ -9,9 +9,10 @@ from glob import glob
 import signal
 import tables as tb
 import pandas as pd
+from traitlets.config import Config
 
 from ctapipe.utils import CutFlow
-from ctapipe.io import EventSource
+from protopipe.pipeline.temp import MySimTelEventSource
 
 from protopipe.pipeline import EventPreparer
 from protopipe.pipeline.utils import (
@@ -222,13 +223,27 @@ def main():
     outfile = tb.open_file(args.outfile, mode="w")
     outTable = {}
     outData = {}
+    
+    # Configuration options for SimTelEventSource
+    # Readout window integration correction
+    try:
+        calib_scale = cfg["Calibration"]["calib_scale"]
+    except KeyError:
+        # defaults for no calibscale applied
+        calib_scale = 1.0
+
+    cfg_SimTelEventSource = Config()
+    cfg_SimTelEventSource.MySimTelEventSource.calib_scale = calib_scale
 
     for i, filename in enumerate(filenamelist):
 
         print("file: {} filename = {}".format(i, filename))
 
-        source = EventSource(
-            input_url=filename, allowed_tels=allowed_tels, max_events=args.max_events
+        source = MyEventSource(
+            input_url=filename,
+            config=cfg_SimTelEventSource,
+            allowed_tels=allowed_tels,
+            max_events=args.max_events
         )
 
         # loop that cleans and parametrises the images and performs the
