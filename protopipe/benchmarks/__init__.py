e4dc9a5e4ea9e99b1edc09bdbd6549901ee45fc7
Michele Peresano <michele.peresano@cea.fr>
2022-01-22 18:54:03 +0100

    Make benchmarks a module

---
+++ protopipe/benchmarks/__init__.py
@@ -0,0 +1,8 @@
+from .operations import compute_weight_BTEL1010
+from .plot import plot_sensitivity_from_pyirf
+
+__all__ = ["compute_weight_BTEL1010",
+           "average_bias_of_charge_resolution",
+           "calculate_RMS_around_1",
+           "prepare_requirements",
+           "plot_sensitivity_from_pyirf"]
