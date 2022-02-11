560e763c2ea6e729e58858a80dc8b254e2fc831c
Michele Peresano <michele.peresano@cea.fr>
2022-02-11 22:08:27 +0100

    Require pandas>=1.0.0 in setup.py

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     install_requires=[
         "ctapipe==0.11.0",
         "pyirf==0.4.0",
-        "pandas",
+        "pandas>=1.0.0",
         "jupyterlab",
         "papermill",
     ],
