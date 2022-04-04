625db476701bf3f9d67c7cf2d6e325ce79d96d21
Karl Kosack <karl.kosack@cea.fr>
2022-04-04 17:42:44 +0200

    added __all__

--- protopipe/benchmarks/plot.py
+++ protopipe/benchmarks/plot.py
@@ -25,6 +25,27 @@ from sklearn.metrics import accuracy_score, auc, roc_curve
 
 LOWER_SIGMA_QUANTILE, UPPER_SIGMA_QUANTILE = norm().cdf([-1, 1])
 
+__all__ = [
+    "plot_profile",
+    "plot_DL1a_reco_true_correlation",
+    "plot_resolution",
+    "plot_bias",
+    "get_single_pixels_spectrum",
+    "plot_sensitivity_from_pyirf",
+    "plot_binned_mean",
+    "plot_binned_median",
+    "plot_hist",
+    "plot_distributions",
+    "plot_roc_curve",
+    "plot_evt_roc_curve_variation",
+    "plot_psf",
+    "plot_background_rate",
+    "BoostedDecisionTreeDiagnostic",
+    "ModelDiagnostic",
+    "RegressorDiagnostic",
+    "ClassifierDiagnostic",
+]
+
 
 def plot_profile(ax, data, xcol, ycol, n_xbin, x_range, logx=False, **kwargs):
     """Plot a profiled histogram.
